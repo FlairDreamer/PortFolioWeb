444792bbd64dd5c5da64d43b7834b9bfcf9fbef2
Flair <flairdreamer@tutanota.com>
2022-04-11 15:25:00 -0300

    Cambios pocos relevantes.

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = []; //TODO: Agregar página de redireccionamiento de cadena vacía y página 404.
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
