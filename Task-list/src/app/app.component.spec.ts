d28515df09c4223703f99cb273aa563737a9349e
Flair <flairdreamer@tutanota.com>
2022-04-12 10:59:57 -0300

    Se agregó Task-list, primera app de pruebas.

--- Task-list/src/app/app.component.spec.ts
+++ Task-list/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular'`, () => {
+  it(`should have as title 'Task-list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('Task-list');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Task-list app is running!');
   });
 });
