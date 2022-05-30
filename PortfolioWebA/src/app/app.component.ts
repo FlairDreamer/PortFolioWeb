6dda4a64b39bfe8b62c198c94494488203464d09
FlairDreamer <flairdreamer@tutanota.com>
2022-05-30 15:23:34 -0300

    Se ha logrado esconder o mostrar algunos componentes en el error 404. Haciendo la barra de regreso del error 404.

--- PortfolioWebA/src/app/app.component.ts
+++ PortfolioWebA/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mi Portafolio Web';
+  constructor(private enrutador: Router) {}
+
+  tieneRuta(ruta: string) { // Esconde los componentes si la ruta es igual a la provista.
+    return this.enrutador.url !== ruta
+  }
 }
